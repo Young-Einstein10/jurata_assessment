937ca858072efcb178c0a7b0b7561784b0efb097
Young-Einstein10 <aabdulrahmanyusuf@gmail.com>
2021-12-29 00:02:05 +0100

    add i18n for dynamic routes

--- pages/[question].tsx
+++ pages/[question].tsx
@@ -1,16 +1,16 @@
-import type {
-  GetServerSideProps,
-  InferGetServerSidePropsType,
-  NextPage,
-} from "next";
+import type { GetServerSideProps, NextPage } from "next";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import HomePage from "../components/HomePage";
 import { AnswerProps } from "../components/HomePage/types";
 import { Layout } from "../components/Layout";
 import { fetchAnswers } from "../utils/apollo-client";
 
 const Question: NextPage = ({ data }: AnswerProps) => {
+  const { t } = useTranslation("common");
+
   return (
-    <Layout>
+    <Layout title={t("title")}>
       <HomePage data={data} />
     </Layout>
   );
@@ -18,8 +18,8 @@ const Question: NextPage = ({ data }: AnswerProps) => {
 
 export default Question;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { question } = context.params as { question: string };
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { question } = ctx.params as { question: string };
 
   const { data } = await fetchAnswers(question);
   const result = { question, ...data?.askMe } as AnswerProps;
@@ -27,6 +27,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
     props: {
       data: result,
+      ...(await serverSideTranslations(ctx.locale!, ["common"])),
     }, // will be passed to the page component as props
   };
 };
